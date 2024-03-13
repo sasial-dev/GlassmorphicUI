7040eb6eec47578a704dc6f29201bbefb8569981
Sasial <44125644+sasial-dev@users.noreply.github.com>
2024-03-13 18:07:58 +1000

    Merge branch 'upstream' into typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,7 @@ interface GlassmorphicUI {
     new(): ImageLabel
     applyGlassToImageLabel(this: void, ImageLabel: ImageLabel): void;
     addGlassBackground(this: void, GuiObject: GuiObject): ImageLabel
+    setDefaultBlurRadius(radius: number): void;
 }
 
 declare const GlassmorphicUI: GlassmorphicUI
